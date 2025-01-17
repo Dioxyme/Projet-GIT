51b83ca9ac96e28beb4babbc1c3c8b2db8ea186c
Ai-nooba <ainaryk66@gmail.com>
2025-01-17 08:46:08 +0100

    Modif index

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+
+<html lang="fr-FR" dir="ltr">
+
+<head>
+
+<title>Top 10 Moyai</title>
+<meta name="description" content=">
+<meta name="author" content=">
+<link rel="stylesheet" href="css/style.css">
+
+</head>
+
+<body>
+
+<header>
+<nav>
+<a href="index.php">Accueil</a>
+<a href="#">Discussions></a>
+<a href="#">A propos</a>
+</nav>
+<h1><a href="index.php"><img src="images/logo.png" alt="logo">Top 10 Moyai</a></h1>
+</header>
+
+<main></main>
+<aside></aside>
+<footer></footer>
+</body>
+</html>
